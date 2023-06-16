da12435030fe63a8c54d6abee95dda3f7a4030f9
Brayan Guimarães dos Santos <brayan.guimaraes.santos@gmail.com>
2023-06-15 22:02:35 -0300

    Aula 03

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,4 +1,5 @@
 import React, { useRef, useState } from "react";
+import VideoFooter from "./components/footer/VideoFooter";
 import "./video.css";
 
 function Video() {
@@ -27,6 +28,8 @@ function Video() {
         loop
         src="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4?t=2023-05-22T19%3A37%3A45.885Z"
       ></video>
+      {/* Side bar */}
+      <VideoFooter />
     </div>
   );
 }
