906ffe4d26ac0e2d1b2298439fb4c142a4fec40d
Brayan Guimarães dos Santos <brayan.guimaraes.santos@gmail.com>
2023-06-18 00:06:26 -0300

    Aula 05

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
+import React, { useEffect, useState } from "react";
 import Video from "./pages/Video";
+import db from "./config/firebase";
+import { collection, getDocs } from "firebase/firestore/lite";
 import "./App.css";
 
 function App() {
+  const [video, setVideos] = useState([]);
+
+  async function getVideos() {
+    const videosCollection = collection(db, "videos");
+    const videosSnapshot = await getDocs(videosCollection);
+    const videosList = videosSnapshot.docs.map((doc) => doc.data());
+
+    setVideos(videosList);
+  }
+
+  useEffect(() => {
+    getVideos();
+  }, []);
+
   return (
     <div className="App">
       <div className="app__videos">
-        <Video
-          likes={111}
-          messages={222}
-          shares={333}
-          name="Brayan"
-          description="Gatinho veloz"
-          music="Musica de suspense"
-          url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4?t=2023-05-22T19%3A37%3A45.885Z"
-        />
-        <Video
-          likes={777}
-          messages={888}
-          shares={999}
-          name="Brayan"
-          description="Gatinho curioso"
-          music="Clap your hands"
-          url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/bird.mp4?t=2023-05-22T19%3A40%3A47.052Z"
-        />
+        {video.map((item) => {
+          return (
+            <Video
+              likes={item.likes}
+              messages={item.messages}
+              shares={item.shares}
+              name={item.name}
+              description={item.description}
+              music={item.music}
+              url={item.url}
+            />
+          );
+        })}
       </div>
     </div>
   );
